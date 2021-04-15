32301b1fef8f6dbc7909b6be46b3909a44c49eea
nnqq <43930873+nnqq@users.noreply.github.com>
2021-04-16 00:29:43 +0300

    fix

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ const proto = require('./proto');
 const logger = require('./logger');
 
 const server = new grpc.Server();
-process.on('SIGTERM', () => {
+
+const sd = () => {
   server.tryShutdown(() => {});
-});
+};
+
+setTimeout(() => sd, 3 * 60 * 60 * 1000);
+process.on('SIGTERM', sd);
 process.on('uncaughtException', (e) => {
   logger.error('uncaughtException', e);
 });
